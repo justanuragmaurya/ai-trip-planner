5a333d79d30d296581531cf74d78a2fb585b0eb6
justanuragmaurya <justanuragmaurya@gmail.com>
2024-07-31 15:31:35 +0530

    removed hardcoded api key and added it from env

--- src/service/FirebaseConfig.jsx
+++ src/service/FirebaseConfig.jsx
@@ -8,7 +8,7 @@ import {getFirestore} from "firebase/firestore"
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyD2FoW5gCUXTR2t5LPtXgroxgDF2zbUMr8",
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
   authDomain: "ai-trip-planner-4f0c5.firebaseapp.com",
   projectId: "ai-trip-planner-4f0c5",
   storageBucket: "ai-trip-planner-4f0c5.appspot.com",
