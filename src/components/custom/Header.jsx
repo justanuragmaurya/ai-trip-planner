4dcfefb25d45b81ae5c2f690648b26cbd1e1e21c
justanuragmaurya <justanuragmaurya@gmail.com>
2024-10-23 01:41:52 +0530

    improved landing page and UI

--- src/components/custom/Header.jsx
+++ src/components/custom/Header.jsx
@@ -1,10 +1,21 @@
 import React from 'react'
+import { Button } from '../ui/button'
 
 function Header() {
   return (
-    <div className='flex justify-between items-center shadow-sm px-5'>
-        <img src="/logo.svg" className='p-5' />
+    <>
+          <div className="flex items-center justify-between px-10 py-3 backdrop-blur-sm sticky top-0 z-10 border-b-[0.5px] border-gray-300">
+      <a href="/">
+        <div className="flex gap-2 items-center">
+          <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQtAXamaSF8es3kqz-aKl6VPD-lMBq8knxS5w&s" height={"30px"} width={"30px"}/>
+          <h2 className="font-bold text-2xl">Yaatra AI</h2>
+        </div>
+      </a>
+      <div className="flex gap-2 items-center">
+        <a href="/create-trip"><Button>Get Started</Button></a>
+      </div>
     </div>
+    </>
   )
 }
 
--- src/components/custom/Hero.jsx
+++ src/components/custom/Hero.jsx
@@ -1,19 +1,41 @@
 import React from 'react'
 import { Button } from '../ui/button'
 import { Link } from 'react-router-dom'
-
+import { cn } from "@/lib/utils";
+import GridPattern from "@/components/ui/grid-pattern";
+import AnimatedGradientText from '../ui/animated-gradient-text';
+ 
 
 
 function Hero() {
   return (
-    <div className='flex flex-col items-center justify-center gap-9 h-[calc(100vh-64px)] w-full'>
+    <div className='flex flex-col items-center justify-center gap-5 h-[calc(100vh-64px)] w-full'>
+      <AnimatedGradientText>
+        🎉 <hr className="mx-2 h-4 w-px shrink-0 bg-gray-300" />{" "}
+        <span
+          className={cn(
+            `inline animate-gradient bg-gradient-to-r from-[#ffaa40] via-[#9c40ff] to-[#ffaa40] bg-[length:var(--bg-size)_100%] bg-clip-text text-transparent`,
+          )}
+        >
+          Introducing Yaatra AI {"→"}
+        </span>
+        
+      </AnimatedGradientText>
       <h1 className='font-extrabold text-[50px] text-center'>
         <span className='text-[#f56551]'>Discover Your Next adventure with AI :<br/> </span>Personalized Iteneriies at your Fingertips</h1>
         <p className='text-gray-500 text-center'>Your personal trip planner and travel curator, creating custom itineraries tailored to your interests and budget.</p>
         <Link to='/create-trip'>
         <Button>Get Started It's Free</Button>
         </Link>
-
+        <GridPattern
+        width={50}
+        height={50}
+        x={-1}
+        y={-1}
+        className={cn(
+          "[mask-image:linear-gradient(to_bottom_right,white,transparent,transparent)] ",
+        )}
+      />
     </div>
   )
 }
